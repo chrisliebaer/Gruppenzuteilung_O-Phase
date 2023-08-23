1b9c8b6ccb1a0b00094111c0f01af293ca708122
Chrisliebär <mail@chrisliebaer.de>
2023-08-23 19:42:21 +0200

    Add Dockerfile for easy experimentation

---
+++ welcome.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# print a welcome message with multiple lines
+cat << EOF
+
+
+# To rerun the 2022 assignment, run the following command:
+GroupAssignment -v 5 -i /source/test_data/data_2022.json -o output.json -c /source/config/config_2022 -t /source/config/types_20220
+
+
+EOF
